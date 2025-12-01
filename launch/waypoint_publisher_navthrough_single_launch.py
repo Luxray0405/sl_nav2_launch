62fc1b1c6f9572968c5b811bf1aa9e1ff309b96c
hEnka11900 <advandynamicsadat17852hq@gmail.com>
2025-12-01 19:56:44 +0900

    param update つくちゃれ遠征3回目

--- launch/waypoint_publisher_navthrough_single_launch.py
+++ launch/waypoint_publisher_navthrough_single_launch.py
@@ -28,8 +28,8 @@ def generate_launch_description():
             parameters=[{
                 'waypoint_file_path': LaunchConfiguration('waypoint_file'),
                 'start_waypoint_index': 0, # ナビゲーションを開始するwaypoint
-                'manual_stop_indices': [25], # 入力待ちするwaypointのindex
-                'stop_area': ["1-3","8-11"] # mid70で障害物停止する区間
+                'manual_stop_indices': [82], # 入力待ちするwaypointのindex
+                'stop_area': ["100-300"] # mid70で障害物停止する区間
             }]
         ),
     ])
