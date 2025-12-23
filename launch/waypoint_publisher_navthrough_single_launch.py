e8aed6a7c2d709dc8d14df61159a438747bc3d83
hEnka11900 <advandynamicsadat17852hq@gmail.com>
2025-12-23 17:30:12 +0900

    つくちゃれ終了後のcommit

--- launch/waypoint_publisher_navthrough_single_launch.py
+++ launch/waypoint_publisher_navthrough_single_launch.py
@@ -27,9 +27,27 @@ def generate_launch_description():
             output='screen',
             parameters=[{
                 'waypoint_file_path': LaunchConfiguration('waypoint_file'),
-                'start_waypoint_index': 0, # ナビゲーションを開始するwaypoint
-                'manual_stop_indices': [500, 1700], # 入力待ちするwaypointのindex
-                'stop_area': ["100-300"] # mid70で障害物停止する区間
+                'start_waypoint_index': 0,              # ナビゲーションを開始するwaypoint
+                'manual_stop_indices': [84,87,90,104,105,106,108], # 入力待ちするwaypointのindex
+                'stop_area': ["101-102"] # mid70で障害物停止する区間  
             }]
         ),
     ])
+
+
+# denchare_long 
+# 'manual_stop_indices': [5, 6, 17, 18],    # 入力待ちするwaypointのindex
+# 'stop_area': ["4-6", "16-18"]             # mid70で障害物停止する区間
+
+# tsukuba_kakunin
+# 'manual_stop_indices': [82],  # 入力待ちするwaypointのindex
+# 'stop_area': ["83-85"]           # mid70で障害物停止する区間
+
+# tsukuba
+# 'manual_stop_indices': [58,61,62,75,76,77,79,127,171,183,184,185,187,202,203], # 入力待ちするwaypointのindex
+# 'stop_area': ["74-76","182-184"] # mid70で障害物停止する区間
+
+# tsukuba_merged
+# 'manual_stop_indices': [84,87,89,102,103,104,106], # 入力待ちするwaypointのindex
+# 'stop_area': ["101-102"] # mid70で障害物停止する区間 
+# # 最初の横断歩道前：80くらい
